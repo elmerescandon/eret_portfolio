a48713075a911dbee142f75b5ae29d0525661387
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-02 15:49:28 -0500

    Add web projects to SecondarySection

--- src/components/templates/SecondarySection.tsx
+++ src/components/templates/SecondarySection.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import AreaSection from '../organisms/AreaSection/AreaSection'
 
 const SecondarySection = () => {
+
+    const webProjects = [
+        <div key={1} >Project 1</div>,
+        <div key={2}>Project 2</div>,
+        <div key={3}>Project 3</div>
+    ]
+
     return (
         <div>
-            <AreaSection title='Web Development' projects={[]} />
+            <AreaSection title='Web Development' projects={[webProjects]} />
             <AreaSection title='Design' projects={[]} />
             <AreaSection title='Academia' projects={[]} />
             <AreaSection title='Personal' projects={[]} />
