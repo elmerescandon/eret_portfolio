74d065bcbf832a736526a099e08ffc0968904527
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-02 13:42:50 -0500

    :truck: Moved files

---
+++ src/components/organisms/Header/HeaderDesktop.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeaderDesktop = () => {
+    return (
+        <div className='max-sm:hidden bg-blue-500 w-full h-16'
+
+        >HeaderDesktop</div>
+    )
+}
+
+export default HeaderDesktop
