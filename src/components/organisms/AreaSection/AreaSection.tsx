7fc21feb17bede45609a2db4739a331a565eb309
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-02 15:49:22 -0500

    Add transition animation to AreaSection component

--- src/components/organisms/AreaSection/AreaSection.tsx
+++ src/components/organisms/AreaSection/AreaSection.tsx
@@ -1,4 +1,5 @@
 import AreaTitle from '@/components/molecules/AreaTitle/AreaTitle'
+import { Transition } from '@headlessui/react'
 import React, { useState } from 'react'
 
 type AreaSectionProps = {
@@ -11,11 +12,24 @@ const AreaSection = ({ title, projects }: AreaSectionProps) => {
     return (
         <div>
             <AreaTitle title={title} extend={extend} setExtend={setExtend} />
-            {projects.map((project, index) => (
-                <div key={index}>
-                    {project}
-                </div>))
-            }
+            <Transition
+                show={extend}
+                appear={true}
+                enter="transition-transform transform duration-500 ease-out"
+                enterFrom="-translate-y-1/2"
+                enterTo="translate-y-0"
+                leave="transition-transform transform duration-500 ease-in"
+                leaveFrom="translate-y-0"
+                leaveTo="-translate-y-1/2"
+            >
+                {projects.map((project, index) => (
+                    <div key={index}>
+                        {project}
+                    </div>))
+                }
+            </Transition>
+
+
         </div>
     )
 }
