fefb8b588131620b11de1d3b61ca8b4a0c49e377
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-02 14:01:59 -0500

    Add Headline component

---
+++ src/components/molecules/Headline/Headline.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Headline = () => {
+    return (
+        <div className="flex flex-col w-full py-4 gap-2">
+            <p className="w-full text-6xl font-bold tracking-tight">Raúl Escandón</p>
+            <p className="w-full text-2xl font-medium">
+                I drive innovation through technology, design, and research, delivering
+                impactful solutions with a data-driven approach for successful outcomes
+                in projects.
+            </p>
+        </div>
+    );
+};
+
+export default Headline;
