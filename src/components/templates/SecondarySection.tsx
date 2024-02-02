a3d9d9ddc81184e3e8cdd4a3a1dd7628363c24d0
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-02 14:23:47 -0500

    Add SecondarySection component

---
+++ src/components/templates/SecondarySection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AreaSection from '../organisms/AreaSection/AreaSection'
+
+const SecondarySection = () => {
+    return (
+        <div>
+            <AreaSection title='Web Development' projects={[]} />
+        </div>
+    )
+}
+
+export default SecondarySection
