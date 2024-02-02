ba645046b49d88f9b538c18e53deb65a070464ca
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-02 14:23:39 -0500

    Add MainSection component

---
+++ src/components/templates/MainSection.tsx
@@ -0,0 +1,14 @@
+import CallToAction from '@/components/molecules/CallToAction/CallToAction'
+import Headline from '@/components/molecules/Headline/Headline'
+import React from 'react'
+
+const MainSection = () => {
+    return (
+        <div>
+            <Headline />
+            <CallToAction />
+        </div>
+    )
+}
+
+export default MainSection
