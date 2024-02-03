3eb15e5b94b4cef569e2705ee87aa7590e6ceec6
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-03 12:49:55 -0500

    Add Technologies enum

---
+++ src/utils/enums/Technologies.ts
@@ -0,0 +1,18 @@
+enum Techonlogies {
+    TYPESCRIPT = 'typeScript',
+    REACT = 'react',
+    MATLAB = 'matlab',
+    C = 'c',
+    TAILWIND = 'tailwind',
+    REDUX = 'redux',
+    NEXTJS = 'nextjs',
+    CPP = 'cpp',
+    JAVASCRIPT = 'javaScript',
+    FIREBASE = 'firebase',
+    ARDUINO = 'arduino',
+    JEST = 'jest',
+    FIGMA = 'figma',
+    SASS = 'sass',
+}
+
+export default Techonlogies;
