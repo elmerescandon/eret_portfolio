4c87e06bb7b289d039c2478ab27aa74f77c657ce
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-03 12:49:43 -0500

    Add image paths for technology icons

---
+++ src/utils/constants.ts
@@ -0,0 +1,19 @@
+import Techonlogies from "./enums/Technologies";
+
+export const imageTechnologies = {
+    [Techonlogies.REACT]: "/technologies/react_icon.svg",
+    [Techonlogies.TYPESCRIPT]: "/technologies/typescript_icon.svg",
+    [Techonlogies.REDUX]: "/technologies/redux_icon.svg",
+    [Techonlogies.SASS]: "/technologies/sass_icon.svg",
+    [Techonlogies.MATLAB]: "/technologies/matlab_icon.svg",
+    [Techonlogies.C]: "/technologies/c_icon.svg",
+    [Techonlogies.TAILWIND]: "/technologies/tailwind_icon.svg",
+    [Techonlogies.NEXTJS]: "/technologies/nextjs_icon.svg",
+    [Techonlogies.CPP]: "/technologies/cpp_icon.svg",
+    [Techonlogies.JAVASCRIPT]: "/technologies/javascript_icon.svg",
+    [Techonlogies.FIREBASE]: "/technologies/firebase_icon.svg",
+    [Techonlogies.ARDUINO]: "/technologies/arduino_icon.svg",
+    [Techonlogies.JEST]: "/technologies/jest_icon.svg",
+    [Techonlogies.FIGMA]: "/technologies/figma_icon.svg",
+};
+
