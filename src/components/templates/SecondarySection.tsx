52f89032660127522afdcd0fa82f857b2f1e0c08
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-03 12:49:37 -0500

    Refactor webProjects in SecondarySection component

--- src/components/templates/SecondarySection.tsx
+++ src/components/templates/SecondarySection.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import AreaSection from '../organisms/AreaSection/AreaSection'
+import webProjects from '@/utils/projects/webProjects'
 
 const SecondarySection = () => {
-
-    const webProjects = [
-        <div key={1} >Project 1</div>,
-        <div key={2}>Project 2</div>,
-        <div key={3}>Project 3</div>
-    ]
-
     return (
         <div>
-            <AreaSection title='Web Development' projects={[webProjects]} />
+            <AreaSection title='Web Development' projects={webProjects} />
             <AreaSection title='Design' projects={[]} />
             <AreaSection title='Academia' projects={[]} />
             <AreaSection title='Personal' projects={[]} />
