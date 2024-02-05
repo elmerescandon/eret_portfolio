7555b947df9cdc66e40ccd95476aad9642187bbb
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-05 17:40:22 -0500

    Add useEffect hook for window resize and update NavBar visibility

--- src/components/templates/PortfolioTemplate.tsx
+++ src/components/templates/PortfolioTemplate.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../organisms/Header/Header'
 import NavigationBar from '../organisms/NavigationBar/NavigationBar';
 import TransitionTemplate from './TransitionTemplate';
@@ -7,16 +7,43 @@ import BodyTemplate from './BodyTemplate';
 import MainSection from './MainSection';
 import SecondarySection from './SecondarySection';
 import Sidebar from '../molecules/SideBar/Sidebar';
+import RailBar from '../organisms/RailBar/RailBar';
 
 const PortfolioTemplate = () => {
-    const [navBar, setNavBar] = useState(false);
+    const getWindowSize = () => {
+        const { innerWidth, innerHeight } = window;
+        return { innerWidth, innerHeight };
+    }
 
+    const [windowSize, setWindowSize] = useState(getWindowSize());
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWindowSize(getWindowSize());
+        }
+
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (windowSize.innerWidth > 1280) {
+            setNavBar(false)
+        }
+    }, [windowSize]);
+
+
+    const [navBar, setNavBar] = useState(false);
     const toggleNavBar = () => {
         setNavBar(!navBar);
     }
     return (
-        <div className='overflow-hidden'>
-            <NavigationBar navBar={navBar} setNavBar={setNavBar} />
+        <div className='flex flex-grow max-xl:overflow-hidden  max-xl:block'>
+            {windowSize.innerWidth <= 1280 && <NavigationBar navBar={navBar} setNavBar={setNavBar} />}
+            <RailBar />
             <TransitionTemplate show={navBar}>
                 <Header toggleNavBar={toggleNavBar} navBar={navBar} />
                 <div className='max-xl:flex max-md:block'>
