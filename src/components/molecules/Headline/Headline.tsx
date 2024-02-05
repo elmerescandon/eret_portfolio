17d9dd0821255222f493730111790e67be0e6bcc
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-05 17:40:08 -0500

    Update Headline component width to max-w-[744px]

--- src/components/molecules/Headline/Headline.tsx
+++ src/components/molecules/Headline/Headline.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Headline = () => {
     return (
-        <div className="flex flex-col w-full py-4 gap-2">
+        <div className="flex flex-col w-full py-4 gap-2 max-w-[744px]">
             <p className="w-full text-6xl font-bold tracking-tight">Raúl Escandón</p>
             <p className="w-full text-2xl font-medium">
                 I drive innovation through technology, design, and research, delivering
