039f5c4d08da12a7f32f9fa82054ce6974e8037f
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-05 17:40:25 -0500

    Refactor TransitionTemplate component

--- src/components/templates/TransitionTemplate.tsx
+++ src/components/templates/TransitionTemplate.tsx
@@ -5,7 +5,9 @@ type TransitionTypes = {
 
 const TransitionTemplate = ({ show, children }: TransitionTypes) => {
     return (
-        <div className={`${show ? `translate-x-2/3 transition duration-300 ease-out` : " translate-x-0 transition duration-300 ease-out"}`}>
+        <div className={`
+        ${show ? `translate-x-2/3 transition duration-300 ease-out` :
+                " translate-x-0 transition duration-300 ease-out"}`}>
             {children}
         </div>
     )
