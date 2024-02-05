0a18116e21b86eed55c4e540ad1fade79210481a
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-05 13:53:41 -0500

    Update HeaderDesktop styling

--- src/components/organisms/Header/HeaderDesktop.tsx
+++ src/components/organisms/Header/HeaderDesktop.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const HeaderDesktop = () => {
     return (
-        <div className='max-sm:hidden bg-blue-500 w-full h-16'
-
-        >HeaderDesktop</div>
+        <div className='max-lg:hidden bg-blue-500 w-full h-16'>
+            HeaderDesktop
+        </div>
     )
 }
 
