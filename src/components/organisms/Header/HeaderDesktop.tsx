8c50b2478b4c23c38ccfd9f3b31acb1f9ec77221
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-05 13:59:57 -0500

    Update HeaderDesktop component to use max-xl breakpoint

--- src/components/organisms/Header/HeaderDesktop.tsx
+++ src/components/organisms/Header/HeaderDesktop.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HeaderDesktop = () => {
     return (
-        <div className='max-lg:hidden bg-blue-500 w-full h-16'>
+        <div className='max-xl:hidden bg-blue-500 w-full h-16'>
             HeaderDesktop
         </div>
     )
