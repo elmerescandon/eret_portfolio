356f13d7dc4b0b676a8bd8ad1965aaed0bb0e5d2
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-02-05 17:56:43 -0500

    Update padding and margin in BodyTemplate component

--- src/components/templates/BodyTemplate.tsx
+++ src/components/templates/BodyTemplate.tsx
@@ -7,7 +7,7 @@ type BodyTemplateProps = {
 const BodyTemplate = ({ children }: BodyTemplateProps) => {
     return (
         <div className='
-        pt-14 px-[80px]
+        pt-14 px-[176px]
         max-xl:pl-[117px] max-xl:pr-8 max-xl:pt-0
         max-lg:px-4'>{children}</div>
     )
