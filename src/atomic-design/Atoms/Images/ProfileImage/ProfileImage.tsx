1c076acb18ddb21d3fcb131a551d58a8e526133d
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-01 23:27:28 -0500

    :sparkles: Add ProfileImage component

---
+++ src/atomic-design/Atoms/Images/ProfileImage/ProfileImage.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React from "react";
+
+const ProfileImage = () => {
+  return (
+    <Image
+      src="/images/profile_picture.svg"
+      alt="profile-picture"
+      className="w-[264px] h-[264px]"
+      width={1000}
+      height={1000}
+    />
+  );
+};
+
+export default ProfileImage;
