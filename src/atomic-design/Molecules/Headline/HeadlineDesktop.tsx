ff3314d5434828d79310758272dd82a7efb90ca0
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-02 12:47:10 -0500

    :sparkles: Add constant headline desktops

--- src/atomic-design/Molecules/Headline/HeadlineDesktop.tsx
+++ src/atomic-design/Molecules/Headline/HeadlineDesktop.tsx
@@ -1,20 +1,12 @@
+import {headlineSubtitle, hepadlineTitle} from "@/utils/constantsHeadline";
 import React from "react";
 
-type HeadlineDesktopProps = {
-  title: string;
-  subtitleContent: {
-    black: string;
-    orange: string;
-    blue: string;
-  };
-};
-
-const HeadlineDesktop = ({title, subtitleContent}: HeadlineDesktopProps) => {
-  const {black, blue, orange} = subtitleContent;
+const HeadlineDesktop = () => {
+  const {black, blue, orange} = headlineSubtitle;
   return (
-    <div className="relative">
+    <div className="relative w-[744px]">
       <div className="gap-4 p-8 border-4 border-black bg-white">
-        <h1 className="text-4xl font-bold">{title}</h1>
+        <h1 className="text-4xl font-bold">{hepadlineTitle}</h1>
         <p className="text-2xl font-bold">
           <span className="text-port-orange">{orange}</span>
           <span className="text-port-black">{black}</span>
