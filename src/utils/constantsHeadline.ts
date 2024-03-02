ed00b933a9e8cfc63edbbb25834ee83c0bc64b15
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-02 12:47:47 -0500

    :sparkles: Add constants for headline title and subtitle

---
+++ src/utils/constantsHeadline.ts
@@ -0,0 +1,7 @@
+export const hepadlineTitle = "Hi, I'm Raúl Escandón";
+
+export const headlineSubtitle = {
+  black: "Full stack developer",
+  orange: "focused on ",
+  blue: " digital product design",
+};
