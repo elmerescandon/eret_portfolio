de5c2c9711ba049756b70463bc973d27b694c9c3
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-05 17:44:02 -0500

    Update bioContent in constantsBio.ts

--- src/utils/constantsBio.ts
+++ src/utils/constantsBio.ts
@@ -3,9 +3,7 @@ import ITechnologiesLabel from "./interfaces/ITechnologies";
 
 export const bioContent = `After working in research across several engineering areas, nowadays I spend my time in creating cutting-edge digital experiences in the web. In the future, I expect to become a digital product designer with a proficient technical background in interaction design.
 
-I have worked building web apps for an outsourcing company and two international audio brands. Now, I lead the technical implementation of a telemedicine application for a healthcare company — Salufy at Aika Corp.
-
-Some of the technologies I have been working recently:`;
+I have worked building web apps for an outsourcing company and two international audio brands. Now, I lead the technical implementation of a telemedicine application for a healthcare company — Salufy at Aika Corp.`;
 
 export const bioTechnologies: ITechnologiesLabel[] = [
   {
