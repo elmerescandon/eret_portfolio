a3a4d39b3291977849ab77ddaa38a99529f6c213
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-05 01:01:24 -0500

    :sparkles: Add WorkExperience component

---
+++ src/atomic-design/Organisms/WorkExperience/WorkExperience.tsx
@@ -0,0 +1,20 @@
+import ExternalLink from "@/atomic-design/Atoms/Links/ExternalLink/ExternalLink";
+import WorkCard from "@/atomic-design/Molecules/WorkCard/WorkCard";
+import workExpreience from "@/utils/information/workExperience";
+import React from "react";
+
+const WorkExperience = () => {
+  return (
+    <div className="flex flex-col justify-start items-start gap-8 max-w-[744px]">
+      <p className="text-2xl font-bold tracking-wide">Work Experience</p>
+      <div>
+        {workExpreience.map((work) => {
+          return <WorkCard workExperience={work} />;
+        })}
+      </div>
+      <ExternalLink href="" title="View Full Resumé" />
+    </div>
+  );
+};
+
+export default WorkExperience;
