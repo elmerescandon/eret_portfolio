bbb97c764bda39a2962ebddcea802bf89e0d590a
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-05 00:55:42 -0500

    :sparkles: Update headline subtitle in constantsHeadline.ts

--- src/utils/constantsHeadline.ts
+++ src/utils/constantsHeadline.ts
@@ -1,7 +1,7 @@
 export const hepadlineTitle = "Hi, I'm Raúl Escandón";
 
 export const headlineSubtitle = {
-  black: "Full stack developer",
-  orange: "focused on ",
+  orange: "Full stack developer",
+  black: " focused on ",
   blue: " digital product design",
 };
