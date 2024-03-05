0d6b7290711528d5f61b8bd82f3beb62b687ed0f
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-05 17:05:03 -0500

    :sparkles: Add Pronouns component

---
+++ src/atomic-design/Molecules/Pronouns/Pronouns.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Pronouns = () => {
+    return (
+        <div className='flex gap-1 w-full justify-start font-semibold pb-2'>
+            <p>Pronouns:</p>
+            <p className='text-port-blue'>He/Him</p>
+        </div>
+    )
+}
+
+export default Pronouns
