547278ba5460f4fc8d9c8731eee18ec33a16fa35
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-05 01:25:03 -0500

    :hammer: Call ILink from their original place

--- src/utils/interfaces/IProject.ts
+++ src/utils/interfaces/IProject.ts
@@ -1,18 +1,13 @@
 import Techonlogies from "../enums/Technologies";
-
-interface ILink {
-    title: string,
-    url: string,
-}
-
+import ILink from "./ILink";
 
 interface IProject {
-    title: string,
-    description: string,
-    launchDate: string,
-    links: ILink[],
-    technologies: Techonlogies[],
-    imagePath: string,
+  title: string;
+  description: string;
+  launchDate: string;
+  links: ILink[];
+  technologies: Techonlogies[];
+  imagePath: string;
 }
 
 export default IProject;
