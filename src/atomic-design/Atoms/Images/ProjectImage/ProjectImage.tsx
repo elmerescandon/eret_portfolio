f26494570324548b8cfb43736dfd1ad07a2c9e80
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-05 13:54:05 -0500

    :lipstick: Add values components

--- src/atomic-design/Atoms/Images/ProjectImage/ProjectImage.tsx
+++ src/atomic-design/Atoms/Images/ProjectImage/ProjectImage.tsx
@@ -1,16 +1,24 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 type ProjectImageProps = {
-    src: string
-    alt: string
-}
+  src: string;
+  alt: string;
+};
 
+const ProjectImage = ({src, alt}: ProjectImageProps) => {
+  return (
+    <div className="relative w-[168px] h-[168px]">
+      <Image
+        src={src}
+        alt={alt}
+        width={1000}
+        height={1000}
+        className="border-4 border-black bg-white w-[168px] h-[168px] object-cover"
+      />
+      <div className="-ml-3 mt-3 absolute w-full h-full bg-port-orange top-0 left-0 -z-10"></div>
+    </div>
+  );
+};
 
-const ProjectImage = ({ src, alt }: ProjectImageProps) => {
-    return (
-        <Image src={src} alt={alt} width={158} height={158} />
-    )
-}
-
-export default ProjectImage
+export default ProjectImage;
