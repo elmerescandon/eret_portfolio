3f6c64525f7a02c8c7243689c9872ee45f5bce90
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-05 01:17:26 -0500

    :sparkles: Update work experience in utils/information/workExperience.ts

--- src/utils/information/workExperience.ts
+++ src/utils/information/workExperience.ts
@@ -10,16 +10,53 @@ const workExpreience: IWork[] = [
                   database and connected the app with three APIs for authentication, 
                   teleconference and payment.`,
     technologies: [
-      Technologies.FIREBASE,
-      Technologies.NEXTJS,
       Technologies.REACT,
-      Technologies.FIGMA,
+      Technologies.TYPESCRIPT,
       Technologies.REDUX,
+      Technologies.NEXTJS,
+      Technologies.FIREBASE,
+      Technologies.TAILWIND,
+      Technologies.FIGMA,
     ],
     time: {
-      startDate: "JUL 2023",
-      endDate: "PRESENT",
+      startDate: "Jul 2023",
+      endDate: "Present",
       duration: "8 months",
+    },
+  },
+  {
+    company: "Black Sheep Peru",
+    position: "eCommerce Developer",
+    description: `Implemented UX/UI practices using frontend for two audio brands: Skullcandy Peru and BlackSheep Audio.
+                  Reduced in 20% the checkout time and doubled (2x) the interaction time on the product page.
+                  Deploy two e-commerce platforms with WordPress and WooCommerce, saving S/.18K in development costs.`,
+    technologies: [Technologies.JAVASCRIPT, Technologies.SASS],
+    time: {
+      startDate: "May 2023",
+      endDate: "Jan 2024",
+      duration: "8 months",
+    },
+  },
+  {
+    company: "Applaudo Studios",
+    position: "React Developer Trainee",
+    description: `Implemented +5 projects using React. Applied SOLID principles and
+                  used fetching data from RESTful APIs. Deployed the projects NextJS and Heroku.
+                  Some used libraries include Axios, Redux Thunk React Router, Jest, SASS, and TailwindCSS.`,
+    technologies: [
+      Technologies.REACT,
+      Technologies.JAVASCRIPT,
+      Technologies.TYPESCRIPT,
+      Technologies.REDUX,
+      Technologies.NEXTJS,
+      Technologies.JEST,
+      Technologies.SASS,
+      Technologies.TAILWIND,
+    ],
+    time: {
+      startDate: "Oct 2022",
+      endDate: "Feb 2023",
+      duration: "4 months",
     },
   },
 ];
