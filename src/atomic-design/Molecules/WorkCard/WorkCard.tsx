5f77f086fe313e1c44a8b950234bf46aebe1621b
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-05 01:01:37 -0500

    :lipstick: Update WorkCard styling

--- src/atomic-design/Molecules/WorkCard/WorkCard.tsx
+++ src/atomic-design/Molecules/WorkCard/WorkCard.tsx
@@ -10,9 +10,9 @@ type WorkCardProps = {
 const WorkCard = ({workExperience}: WorkCardProps) => {
   const {time, company, position, description, technologies} = workExperience;
   return (
-    <div>
+    <div className="flex flex-wrap gap-6 ">
       <WorkExperienceTime time={time} />
-      <div className="flex flex-col items-start gap-4">
+      <div className="flex flex-col items-start gap-4 max-w-[552px]">
         <p className="text-base font-semibold">{`${position} · ${company} `}</p>
         <p className="text-base font-normal">{description}</p>
         <ProjectTechnologies technologies={technologies} />
