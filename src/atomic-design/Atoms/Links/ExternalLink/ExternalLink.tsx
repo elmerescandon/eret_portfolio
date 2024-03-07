9bf4c873e866fcfb62ac3f6bbbcab391a842d008
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-07 15:52:34 -0500

    :lipstick: Add animation to external link

--- src/atomic-design/Atoms/Links/ExternalLink/ExternalLink.tsx
+++ src/atomic-design/Atoms/Links/ExternalLink/ExternalLink.tsx
@@ -1,19 +1,51 @@
-import { ArrowUpRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import {ArrowUpRightIcon} from "@heroicons/react/24/outline";
+import Link from "next/link";
+import React, {useState} from "react";
 
 type ExternalLinkProps = {
-    href: string
-    title: string;
-}
+  href: string;
+  title: string;
+};
 
-const ExternalLink = ({ href, title }: ExternalLinkProps) => {
-    return (
-        <Link href={href} className='flex items-center gap-4'>
-            <p className='font-semibold text-base'>{title}</p>
-            <ArrowUpRightIcon height={24} width={24} />
-        </Link>
-    )
-}
+const ExternalLink = ({href, title}: ExternalLinkProps) => {
+  const [isHovered, setIsHovered] = useState(false);
 
-export default ExternalLink
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+  return (
+    <Link
+      href={href}
+      className="flex items-center gap-1"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <p
+        className="text-base"
+        style={{
+          fontWeight: isHovered ? "bold" : "normal",
+          transition: "font-weight 0.2s",
+        }}
+      >
+        {title}
+      </p>
+      <ArrowUpRightIcon
+        height={16}
+        width={16}
+        style={{
+          width: isHovered ? "20px" : "16px",
+          height: isHovered ? "20px" : "16px",
+          marginTop: isHovered ? "-5px" : "0",
+          transition: "width 0.2s, height 0.2s, margin-top 0.2s",
+        }}
+      />
+    </Link>
+  );
+};
+
+export default ExternalLink;
