99e77be45e551512bc62d902be85bf702d484e54
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-06 19:09:03 -0500

    :sparkles: Refactor PageSectionGroup component to include sectionReferences

--- src/atomic-design/Organisms/PageSectionGroup/PageSectionGroup.tsx
+++ src/atomic-design/Organisms/PageSectionGroup/PageSectionGroup.tsx
@@ -3,13 +3,23 @@ import React from "react";
 
 type PageSectionGroupProps = {
   sections: string[];
+  sectionReferences: React.RefObject<HTMLDivElement | null>[];
 };
 
-const PageSectionGroup = ({sections}: PageSectionGroupProps) => {
+const PageSectionGroup = ({
+  sections,
+  sectionReferences,
+}: PageSectionGroupProps) => {
   return (
     <div className="flex flex-col justify-start items-start gap-8">
       {sections.map((section, index) => {
-        return <PageSection sectionName={section} key={index} />;
+        return (
+          <PageSection
+            sectionName={section}
+            key={index}
+            sectionReference={sectionReferences[index]}
+          />
+        );
       })}
     </div>
   );
