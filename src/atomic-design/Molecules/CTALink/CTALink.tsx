11c328c2fc13c014a37b7ec723660e19dd8c19df
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-07 16:03:30 -0500

    Refactor CTALink component to add hover effect

--- src/atomic-design/Molecules/CTALink/CTALink.tsx
+++ src/atomic-design/Molecules/CTALink/CTALink.tsx
@@ -1,22 +1,42 @@
+"use client";
 import CTAIcons from "@/atomic-design/Atoms/Icons/CTAIcons/CTAIcons";
 import ICallToAction from "@/utils/interfaces/ICallToAction";
-import Link from "next/link";
-import React from "react";
+import React, {useState} from "react";
 
 type CTALinkProps = {
   cta: ICallToAction;
 };
 
 const CTALink = ({cta}: CTALinkProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
   const {icon, link} = cta;
   const {url, title} = link;
   return (
-    <div className="w-full flex justify-between">
+    <a
+      className="w-full flex justify-between items-center"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      href={url}
+      target="_blank"
+    >
       <CTAIcons callToAction={icon} />
-      <a href={url} target="_blank">
+      <p
+        style={{
+          fontWeight: isHovered ? "bold" : "normal",
+          transition: "font-weight 0.2s",
+        }}
+      >
         {title}
-      </a>
-    </div>
+      </p>
+    </a>
   );
 };
 
