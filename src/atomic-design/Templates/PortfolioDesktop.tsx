6e54ffb4e36fd39ccd89477bb010534a2a499695
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-12 16:51:12 -0500

    :sparkles: Add INavigationRefs import and refactor component references

--- src/atomic-design/Templates/PortfolioDesktop.tsx
+++ src/atomic-design/Templates/PortfolioDesktop.tsx
@@ -4,6 +4,7 @@ import BioGeneral from "../Organisms/BioGeneral/BioGeneral";
 import WorkExperience from "../Organisms/WorkExperience/WorkExperience";
 import ProjectExperience from "../Organisms/ProjectExperience/ProjectExperience";
 import NavigationBarDesktop from "../Organisms/NavigationBar/NavigationBarDesktop";
+import INavigationRefs from "@/utils/interfaces/INavigationRefs";
 
 const PortfolioDesktop = () => {
   const viewAbout = useRef<HTMLDivElement | null>(null);
@@ -19,17 +20,9 @@ const PortfolioDesktop = () => {
         max-2xl:pl-80
         max-xl:pl-0"
       >
-        <div ref={viewAbout}>
-          <BioGeneral />
-        </div>
-
-        <div ref={viewExperience}>
-          <WorkExperience />
-        </div>
-
-        <div ref={viewProjects}>
-          <ProjectExperience />
-        </div>
+        <BioGeneral ref={viewAbout} />
+        <WorkExperience ref={viewExperience} />
+        <ProjectExperience ref={viewProjects} />
       </div>
     </div>
   );
