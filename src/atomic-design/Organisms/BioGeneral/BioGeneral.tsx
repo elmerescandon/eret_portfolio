87679f362c37d6af836289ce8f89762d0aeadca0
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-12 16:51:04 -0500

    :sparkles: Refactor BioGeneral component to use forwardRef

--- src/atomic-design/Organisms/BioGeneral/BioGeneral.tsx
+++ src/atomic-design/Organisms/BioGeneral/BioGeneral.tsx
@@ -1,22 +1,23 @@
-import React from "react";
+import React, {forwardRef} from "react";
 import BioInformation from "../BioInformation/BioInformation";
 import {bioContent, bioTechnologies} from "@/utils/constantsBio";
 import ProfileImage from "@/atomic-design/Atoms/Images/ProfileImage/ProfileImage";
 import HeadlineDesktop from "@/atomic-design/Molecules/Headline/HeadlineDesktop";
 
-const BioGeneral = () => {
-  return (
-    <div
-      className="flex flex-col gap-16 justify-center
+const BioGeneral = forwardRef<HTMLDivElement, {}>((props, ref) => (
+  <div
+    ref={ref}
+    className="flex flex-col gap-16 justify-center
       max-w-[744px] h-screen min-h-full"
-    >
-      <HeadlineDesktop />
-      <div className="flex items-center justify-start gap-6">
-        <BioInformation content={bioContent} technologies={bioTechnologies} />
-        <ProfileImage />
-      </div>
+  >
+    <HeadlineDesktop />
+    <div className="flex items-center justify-start gap-6">
+      <BioInformation content={bioContent} technologies={bioTechnologies} />
+      <ProfileImage />
     </div>
-  );
-};
+  </div>
+));
+
+BioGeneral.displayName = "BioGeneral";
 
 export default BioGeneral;
