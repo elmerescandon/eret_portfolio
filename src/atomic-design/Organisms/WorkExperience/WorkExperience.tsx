08f4c702556f9c784271f2a640d5bce451317c6b
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-12 16:51:20 -0500

    :sparkles: Refactor WorkExperience component to use forwardRef

--- src/atomic-design/Organisms/WorkExperience/WorkExperience.tsx
+++ src/atomic-design/Organisms/WorkExperience/WorkExperience.tsx
@@ -1,11 +1,12 @@
 import ExternalLink from "@/atomic-design/Atoms/Links/ExternalLink/ExternalLink";
 import WorkCard from "@/atomic-design/Molecules/WorkCard/WorkCard";
 import workExpreience from "@/utils/information/workExperience";
-import React from "react";
+import React, {forwardRef} from "react";
 
-const WorkExperience = () => {
+const WorkExperience = forwardRef<HTMLDivElement, {}>((props, ref) => {
   return (
     <div
+      ref={ref}
       className="flex flex-col justify-center items-start gap-8 
       max-w-[744px] w-full h-screen min-h-full"
     >
@@ -21,6 +22,8 @@ const WorkExperience = () => {
       />
     </div>
   );
-};
+});
+
+WorkExperience.displayName = "WorkExperience";
 
 export default WorkExperience;
