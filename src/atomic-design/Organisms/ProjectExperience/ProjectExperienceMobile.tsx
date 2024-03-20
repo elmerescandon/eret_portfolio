d45b53aae6a4c2ccb8b422dc42672ad88d587dc2
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-20 17:51:50 -0500

    :sparkles: Add ProjectExperienceMobile component

---
+++ src/atomic-design/Organisms/ProjectExperience/ProjectExperienceMobile.tsx
@@ -0,0 +1,23 @@
+import ExternalLink from "@/atomic-design/Atoms/Links/ExternalLink/ExternalLink";
+import Headline from "@/atomic-design/Atoms/Typography/Headline";
+import ProjectCard from "@/atomic-design/Molecules/ProjectCard/ProjectCard";
+import webProjects from "@/utils/information/webProjects";
+
+const ProjectExperienceMobile = () => {
+  return (
+    <div
+      className="flex flex-col justify-center items-start gap-8 
+          max-w-[744px] w-full"
+    >
+      <Headline type="small">Projects</Headline>
+      <div className="flex flex-col gap-6 w-full">
+        {webProjects.map((project, index) => {
+          return <ProjectCard size="mobile" project={project} key={index} />;
+        })}
+      </div>
+      <ExternalLink href="" title="View all projects" />
+    </div>
+  );
+};
+
+export default ProjectExperienceMobile;
