053972ba3c7894f48d575635256d59aa40cb51bc
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-20 15:38:43 -0500

    :sparkles: Refactor WorkCard component to support different sizes

--- src/atomic-design/Molecules/WorkCard/WorkCard.tsx
+++ src/atomic-design/Molecules/WorkCard/WorkCard.tsx
@@ -1,24 +1,19 @@
 import IWork from "@/utils/interfaces/IWork";
-import React from "react";
-import WorkExperienceTime from "../WorkTime/WorkTime.tsx";
-import ProjectTechnologies from "../ProjectTechnologies/ProjectTechnologies";
+import WorkCardDesktop from "./WorkCardDesktop";
+import WorkCardMobile from "./WorkCardMobile";
 
 type WorkCardProps = {
   workExperience: IWork;
+  size: "desktop" | "mobile";
 };
 
-const WorkCard = ({workExperience}: WorkCardProps) => {
-  const {time, company, position, description, technologies} = workExperience;
-  return (
-    <div className="flex flex-wrap gap-6 ">
-      <WorkExperienceTime time={time} />
-      <div className="flex flex-col items-start gap-4 max-w-[552px]">
-        <p className="text-base font-semibold">{`${position} · ${company} `}</p>
-        <p className="text-base font-normal">{description}</p>
-        <ProjectTechnologies technologies={technologies} />
-      </div>
-    </div>
-  );
+const WorkCard = ({workExperience, size}: WorkCardProps) => {
+  switch (size) {
+    case "desktop":
+      return <WorkCardDesktop workExperience={workExperience} />;
+    case "mobile":
+      return <WorkCardMobile workExperience={workExperience} />;
+  }
 };
 
 export default WorkCard;
