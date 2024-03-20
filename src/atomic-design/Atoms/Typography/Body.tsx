6733ad4fb63d436cf62f2a569d60914853fd3d8e
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-20 12:19:22 -0500

    :sparkles: Add Body component for typography

---
+++ src/atomic-design/Atoms/Typography/Body.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+type BodyProps = {
+  children: React.ReactNode;
+  type: "large" | "medium" | "small";
+  button?: boolean;
+  other?: string;
+};
+
+const Body = ({
+  children,
+  type,
+  button = false,
+  other = "bg-basic-black",
+}: BodyProps) => {
+  const classLarge = `text-[16px]  leading-[24px] tracking-[+0.5px] 
+    ${button ? "font-semibold" : "font-light"}`;
+  const classMedium = `text-[14px] leading-[20px] tracking-[+0.25px] 
+    ${button ? "font-semibold" : "font-light"}`;
+  const classSmall = `text-[12px] leading-[16px] tracking-[+0.4px] 
+    ${button ? "font-semibold" : "font-light"}`;
+
+  let className;
+  switch (type) {
+    case "large":
+      className = classLarge;
+      break;
+    case "medium":
+      className = classMedium;
+      break;
+    case "small":
+      className = classSmall;
+      break;
+    default:
+      className = "";
+  }
+
+  return <p className={`${className} ${other}`}>{children}</p>;
+};
+
+export default Body;
