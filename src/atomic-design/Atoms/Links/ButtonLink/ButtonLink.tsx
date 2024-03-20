a266fd18b7795ad2ac2b400557700bc5fbbf6c27
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-20 13:06:58 -0500

    :sparkles: Add Body component to ButtonLink

--- src/atomic-design/Atoms/Links/ButtonLink/ButtonLink.tsx
+++ src/atomic-design/Atoms/Links/ButtonLink/ButtonLink.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import Body from "../../Typography/Body";
 
 type ButtonLinkProps = {
   href: string;
@@ -13,6 +14,9 @@ const ButtonLink = ({href, text}: ButtonLinkProps) => {
       className="bg-port-blue text-white border rounded-xl
     hover:bg-white hover:text-port-blue hover:border-port-blue text-base font-semibold"
     >
+      <Body type="large" button other="w-full h-full">
+        {text}
+      </Body>
       {text}
     </Link>
   );
