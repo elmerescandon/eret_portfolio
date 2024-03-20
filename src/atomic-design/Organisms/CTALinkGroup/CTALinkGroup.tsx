cc236fbe11d9743f6bd4eabb1b413dd8cbdff6f6
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-20 17:25:34 -0500

    :hammer: Add size prop to CTALinkGroup component

--- src/atomic-design/Organisms/CTALinkGroup/CTALinkGroup.tsx
+++ src/atomic-design/Organisms/CTALinkGroup/CTALinkGroup.tsx
@@ -4,13 +4,14 @@ import React from "react";
 
 type CTALinkGroupProps = {
   callToActionArray: ICallToAction[];
+  size: "tablet" | "desktop";
 };
 
-const CTALinkGroup = ({callToActionArray}: CTALinkGroupProps) => {
+const CTALinkGroup = ({callToActionArray, size}: CTALinkGroupProps) => {
   return (
     <div className="flex flex-col gap-4 justify-start items-start">
       {callToActionArray.map((cta, index) => {
-        return <CTALink cta={cta} key={index} />;
+        return <CTALink size={size} cta={cta} key={index} />;
       })}
     </div>
   );
