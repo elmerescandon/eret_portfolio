f19c3546bc35de89ddf27bc97140de85cd06378f
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-20 13:12:11 -0500

    :sparkles: Refactor TechnologyLabel component to use Body component for technology title

--- src/atomic-design/Molecules/TechnologyLabel/TechnologyLabel.tsx
+++ src/atomic-design/Molecules/TechnologyLabel/TechnologyLabel.tsx
@@ -1,19 +1,22 @@
 import TechnologyIcons from "@/atomic-design/Atoms/Icons/TechnologyIcons/TechnologyIcons";
+import Body from "@/atomic-design/Atoms/Typography/Body";
 import React from "react";
 
 type TechnologyLabelProps = {
   technology: string;
   technologyTitle: string;
+  size: "big" | "small";
 };
 
 const TechnologyLabel = ({
   technology,
   technologyTitle,
+  size,
 }: TechnologyLabelProps) => {
   return (
     <div className="flex gap-2 items-center">
-      <TechnologyIcons technology={technology} />
-      <p className="text-base font-semibold">{technologyTitle}</p>
+      <TechnologyIcons technology={technology} size={size} />
+      <Body type={size === "big" ? "large" : "small"}>{technologyTitle}</Body>
     </div>
   );
 };
