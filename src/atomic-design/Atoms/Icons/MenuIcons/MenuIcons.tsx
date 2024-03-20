db27993fcef6f80bf32da8ea21d89e9272599089
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-20 14:56:30 -0500

    :sparkles: Refactor MenuIcons component to use Heroicons

--- src/atomic-design/Atoms/Icons/MenuIcons/MenuIcons.tsx
+++ src/atomic-design/Atoms/Icons/MenuIcons/MenuIcons.tsx
@@ -1,18 +1,40 @@
-import Image from "next/image";
+import {
+  BriefcaseIcon,
+  CodeBracketSquareIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
 import React from "react";
 
 type MenuIconsProps = {
   icon: "briefcase" | "terminal" | "user";
+  other?: string;
 };
 
-export const MenuIcons = ({icon}: MenuIconsProps) => {
-  return (
-    <Image
-      className="w-8 h-8 text-black"
-      src={`/icons/menu/${icon}.svg`}
-      alt={icon}
-      width={32}
-      height={32}
-    />
-  );
+export const MenuIcons = ({icon, other = "text-black"}: MenuIconsProps) => {
+  switch (icon) {
+    case "briefcase":
+      return (
+        <BriefcaseIcon
+          className={`w-8 h-8 ${other} stroke-2`}
+          width={32}
+          height={32}
+        />
+      );
+    case "terminal":
+      return (
+        <CodeBracketSquareIcon
+          className={`w-8 h-8 ${other} stroke-2`}
+          width={32}
+          height={32}
+        />
+      );
+    case "user":
+      return (
+        <UserIcon
+          className={`w-8 h-8 ${other} stroke-2`}
+          width={32}
+          height={32}
+        />
+      );
+  }
 };
