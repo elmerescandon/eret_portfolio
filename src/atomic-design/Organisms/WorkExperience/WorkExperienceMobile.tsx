948c5baa5cf7df3f1d2481e34a978cf281663886
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-20 18:23:06 -0500

    :sparkles: Add WorkExperienceMobile component

---
+++ src/atomic-design/Organisms/WorkExperience/WorkExperienceMobile.tsx
@@ -0,0 +1,27 @@
+import ExternalLink from "@/atomic-design/Atoms/Links/ExternalLink/ExternalLink";
+import Headline from "@/atomic-design/Atoms/Typography/Headline";
+import WorkCard from "@/atomic-design/Molecules/WorkCard/WorkCard";
+import workExpreience from "@/utils/information/workExperience";
+import React from "react";
+
+const WorkExperienceMobile = () => {
+  return (
+    <div
+      className="flex flex-col justify-center items-start gap-8 
+    max-w-[744px]"
+    >
+      <Headline type="small">Work Experience</Headline>
+      <div className="flex flex-col gap-6">
+        {workExpreience.map((work, index) => {
+          return <WorkCard size="mobile" workExperience={work} key={index} />;
+        })}
+      </div>
+      <ExternalLink
+        href="https://drive.google.com/file/d/1knuHTgCn8n1_Spk7uVWn4oyTv58OouvD/view?usp=sharing"
+        title="View Full Resumé"
+      />
+    </div>
+  );
+};
+
+export default WorkExperienceMobile;
