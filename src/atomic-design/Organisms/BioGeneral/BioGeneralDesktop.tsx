5364634293d60057d39ebe39854764bb8a04b277
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-20 19:13:24 -0500

    :lipstck: Update BioGeneralDesktop styling

--- src/atomic-design/Organisms/BioGeneral/BioGeneralDesktop.tsx
+++ src/atomic-design/Organisms/BioGeneral/BioGeneralDesktop.tsx
@@ -4,8 +4,12 @@ import {bioTechnologies} from "@/utils/constantsBio";
 
 const BioGeneralDesktop = () => (
   <div
-    className="flex flex-col gap-16 justify-center
-        max-w-[744px] h-screen min-h-full"
+    className="flex flex-col gap-16 justify-center min-h-full
+        w-full max-w-[880px] 
+        min-[1440px]:h-screen 
+        max-[905px]:max-w-[756px]
+        max-[600px]:max-w-[480px]
+        "
   >
     <Headline size="desktop" />
     <BioInformation size="desktop" technologies={bioTechnologies} />
