cbf82f03abf9de2fbc7981c5e520443c49f7cbfa
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-21 07:01:48 -0500

    :sparkles: Add email functionality to mobile navigation bar

--- src/atomic-design/Organisms/NavigationBar/NavigationBarMobile.tsx
+++ src/atomic-design/Organisms/NavigationBar/NavigationBarMobile.tsx
@@ -1,7 +1,23 @@
+import MenuButton from "@/atomic-design/Molecules/MenuButton/MenuButton";
+import {useRouter} from "next/navigation";
 import React from "react";
 
 const NavigationBarMobile = () => {
-  return <div>NavigationBarMobile</div>;
+  const router = useRouter();
+
+  const handleClickEmail = () => {
+    router.push("mailto:elmer.escandontufino@gmail.com");
+  };
+
+  return (
+    <div>
+      <div className="absolute z-10 bg-white h-14 px-4 py-3 w-full flex justify-between">
+        <MenuButton icon="menu" onClick={() => {}} />
+        <MenuButton icon="atom" onClick={() => {}} />
+        <MenuButton icon="email" onClick={handleClickEmail} />
+      </div>
+    </div>
+  );
 };
 
 export default NavigationBarMobile;
