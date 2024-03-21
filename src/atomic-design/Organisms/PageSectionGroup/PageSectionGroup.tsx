c92bbb239520dcd8b0d32dd9c7f2a3e91e2dcc0b
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-21 07:26:18 -0500

    :sparkles: Add mobile support to PageSectionGroup

--- src/atomic-design/Organisms/PageSectionGroup/PageSectionGroup.tsx
+++ src/atomic-design/Organisms/PageSectionGroup/PageSectionGroup.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import PageSectionGroupDesktop from "./PageSectionGroupDesktop";
 import PageSectionGroupTablet from "./PageSectionGroupTablet";
 import {navigationSections} from "@/utils/constantsNavigation";
+import PageSectionGroupMobile from "./PageSectionGroupMobile";
 
 type PageSectionGroupProps = {
   sections?: string[];
   sectionReferences: React.RefObject<HTMLDivElement | null>[];
-  type: "desktop" | "tablet";
+  type: "desktop" | "tablet" | "mobile";
 };
 
 const PageSectionGroup = ({
@@ -24,6 +25,13 @@ const PageSectionGroup = ({
       );
     case "tablet":
       return <PageSectionGroupTablet sectionReferences={sectionReferences} />;
+    case "mobile":
+      return (
+        <PageSectionGroupMobile
+          sections={sections}
+          sectionReferences={sectionReferences}
+        />
+      );
   }
 };
 
