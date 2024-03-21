3d11b9698db0d67c1a46290274d7866fcdfbe159
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-21 07:01:35 -0500

    :sparkles: Refactor MenuButton component

--- src/atomic-design/Molecules/MenuButton/MenuButton.tsx
+++ src/atomic-design/Molecules/MenuButton/MenuButton.tsx
@@ -8,8 +8,11 @@ type MenuButtonProps = {
 
 const MenuButton = ({icon, onClick}: MenuButtonProps) => {
   return (
-    <button onClick={onClick} className="w-9 h-9 bg-white">
-      <MobileMenuIcons icon="menu" />
+    <button
+      onClick={onClick}
+      className="w-9 h-9 bg-white active:traslate-y-0.5 active:shadow-md rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 transition-transform duration-300 ease-in-out"
+    >
+      <MobileMenuIcons icon={icon} />
     </button>
   );
 };
