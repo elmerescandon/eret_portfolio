7397e2288f30828fe9f2e20ab4bedcf659d3b59a
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-21 07:26:01 -0500

    :sparkles: Add SideBar component and handle menu toggle

--- src/atomic-design/Templates/PortfolioDesktop.tsx
+++ src/atomic-design/Templates/PortfolioDesktop.tsx
@@ -1,31 +1,40 @@
 "use client";
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import BioGeneral from "../Organisms/BioGeneral/BioGeneral";
 import WorkExperience from "../Organisms/WorkExperience/WorkExperience";
 import ProjectExperience from "../Organisms/ProjectExperience/ProjectExperience";
 import NavigationBar from "../Organisms/NavigationBar/NavigationBar";
+import SideBar from "../Organisms/SideBar/SideBar";
 
 const PortfolioDesktop = () => {
+  const [openMenu, setOpenMenu] = useState(false);
   const viewAbout = useRef<HTMLDivElement | null>(null);
   const viewExperience = useRef<HTMLDivElement | null>(null);
   const viewProjects = useRef<HTMLDivElement | null>(null);
   return (
-    <div className="relative w-full h-full">
-      <NavigationBar
+    <div>
+      <SideBar
+        isOpen={openMenu}
         sectionReferences={[viewAbout, viewExperience, viewProjects]}
       />
-      <div
-        className="flex flex-col gap-16 w-full items-center 
+      <div className="relative w-full h-full">
+        <NavigationBar
+          sectionReferences={[viewAbout, viewExperience, viewProjects]}
+          handleOpenMenu={() => setOpenMenu(!openMenu)}
+        />
+        <div
+          className="flex flex-col gap-16 w-full items-center 
         
         max-[1600px]:pl-32 
         max-[1280px]:px-[117px]
         max-[905px]:pr-8
         max-[600px]:pl-[88px]
         max-[360px]:px-4 max-[360px]:pt-20"
-      >
-        <BioGeneral ref={viewAbout} />
-        <WorkExperience ref={viewExperience} />
-        <ProjectExperience ref={viewProjects} />
+        >
+          <BioGeneral ref={viewAbout} />
+          <WorkExperience ref={viewExperience} />
+          <ProjectExperience ref={viewProjects} />
+        </div>
       </div>
     </div>
   );
