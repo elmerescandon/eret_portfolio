0aa72c42e131153345964565d3862cea2639bc72
Raul Escandon <elmer.escandon@utec.edu.pe>
2024-03-21 07:09:18 -0500

    :sparkles: Add Pronouns component and CTALinkGroup to NavigationBarMobile

--- src/atomic-design/Organisms/NavigationBar/NavigationBarMobile.tsx
+++ src/atomic-design/Organisms/NavigationBar/NavigationBarMobile.tsx
@@ -1,21 +1,30 @@
 import MenuButton from "@/atomic-design/Molecules/MenuButton/MenuButton";
+import Pronouns from "@/atomic-design/Molecules/Pronouns/Pronouns";
 import {useRouter} from "next/navigation";
-import React from "react";
+import React, {useState} from "react";
+import CTALinkGroup from "../CTALinkGroup/CTALinkGroup";
+import {callToActionArray} from "@/utils/constantsNavigation";
 
-const NavigationBarMobile = () => {
+type NavigationBarMobileProps = {
+  handleOpenMenu: () => void;
+};
+
+const NavigationBarMobile = ({handleOpenMenu}: NavigationBarMobileProps) => {
   const router = useRouter();
+  const [openMenu, setOpenMenu] = useState(false);
 
   const handleClickEmail = () => {
     router.push("mailto:elmer.escandontufino@gmail.com");
   };
 
   return (
-    <div>
-      <div className="absolute z-10 bg-white h-14 px-4 py-3 w-full flex justify-between">
-        <MenuButton icon="menu" onClick={() => {}} />
-        <MenuButton icon="atom" onClick={() => {}} />
-        <MenuButton icon="email" onClick={handleClickEmail} />
-      </div>
+    <div className="absolute z-10 bg-white h-14 px-4 py-3 w-full flex justify-between">
+      <MenuButton
+        icon={`${openMenu ? "close" : "menu"}`}
+        onClick={handleOpenMenu}
+      />
+      <MenuButton icon="atom" onClick={() => {}} />
+      <MenuButton icon="email" onClick={handleClickEmail} />
     </div>
   );
 };
